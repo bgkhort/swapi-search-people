2e94e181740c91a3b571a879ed4c280bd0d360ab
Bogdan Khort <61352305+bgkhort@users.noreply.github.com>
2022-07-06 11:16:50 +0300

    fix url service

--- src/services/urlService.js
+++ src/services/urlService.js
@@ -2,10 +2,10 @@ class UrlService {
   static getURLsByFilter(filters, filterFields) {
     const result = [];
 
-    for (let filterField in filters) {
-      if (filters[filterField].length) {
-        filters[filterField].forEach((name) => {
-          const fieldName = filterFields[filterField].find(
+    for (let title in filters) {
+      if (filters[title].length) {
+        filters[title].forEach((name) => {
+          const fieldName = filterFields[title].find(
             (item) => item.title === name
           );
           result.push(fieldName.items);
